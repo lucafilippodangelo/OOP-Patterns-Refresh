abed1bb995701dc3caae6d3df496c63c5cb95db9
Luca D'Angelo <info@lucadangelo.it>
2017-11-26 16:58:36 +0000

    -

---
+++ PatternsRefresh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternsRefresh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
