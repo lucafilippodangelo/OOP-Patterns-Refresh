a026517c1bc16d0130fa1e08096a3739950f244e
Luca D'Angelo <info@lucadangelo.it>
2018-03-18 17:44:20 +0000

    readme

--- PatternsRefresh/Program.cs
+++ PatternsRefresh/Program.cs
@@ -7,30 +7,29 @@ namespace PatternsRefresh
     {
         static void Main(string[] args)
         {
-            bool RunMediatorDesignPattern = true;
-            
-            //LD STEP006
-            #region Mediator Design Pattern
-            if (RunMediatorDesignPattern)
-            { 
-                Mediator lucaMediator = new Mediator(); //LD creation instance of "Mediator"
-                
-                ColleagueOne c1 = new ColleagueOne(lucaMediator); //LD creation of concrete instance of "Coleague", need to pass "Mediator" instance
-                lucaMediator.c1 = c1; //LD set "c1" in concrete "Mediator" instance
+            RunMediatorDesignPattern();
 
-                ColleagueTwo c2 = new ColleagueTwo(lucaMediator);
-                lucaMediator.c2 = c2;
 
-                ColleagueThree c3 = new ColleagueThree(lucaMediator);
-                lucaMediator.c3 = c3;
-
-                //LD test of "Mediator" logic
-                c1.Send("msg001");
-                c2.Send("msg002");
-
-                Console.ReadLine();
-            }
-            #endregion
         }
-    }
-}
+
+        public static void RunMediatorDesignPattern() {
+            Mediator lucaMediator = new Mediator(); //LD creation instance of "Mediator"
+
+            ColleagueOne c1 = new ColleagueOne(lucaMediator); //LD creation of concrete instance of "Coleague", need to pass "Mediator" instance
+            lucaMediator.c1 = c1; //LD set "c1" in concrete "Mediator" instance
+
+            ColleagueTwo c2 = new ColleagueTwo(lucaMediator);
+            lucaMediator.c2 = c2;
+
+            ColleagueThree c3 = new ColleagueThree(lucaMediator);
+            lucaMediator.c3 = c3;
+
+            //LD test of "Mediator" logic
+            c1.Send("msg001");
+            c2.Send("msg002");
+
+            Console.ReadLine();
+        }
+
+    }//LD end Program
+}//LD close namespace
