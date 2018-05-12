644aebb2719e19cf572c7ed1d84ea3ff4dd4d574
Luca D'Angelo <info@lucadangelo.it>
2018-05-12 19:58:25 +0100

    observer done

---
+++ ObserverBehavioralPattern/IObserver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverBehavioralPattern
+{
+    public interface IObserver<T>
+    {
+        void Update(T subjectState);
+        //T GetState();
+    }
+}
