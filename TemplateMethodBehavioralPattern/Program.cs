632a3b766236d0c80b52f6b62d3bbe5621f8e953
Luca D'Angelo <info@lucadangelo.it>
2018-05-13 13:04:14 +0100

    template method

---
+++ TemplateMethodBehavioralPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemplateMethodBehavioralPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
